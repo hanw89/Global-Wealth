6ffad9a8a60fbdab9733a214e514ba95b476b556
hanw89 <hanw89@gmail.com>
2026-05-03 06:47:09 +0000

    chore: optimize for Cloudflare Pages with BrowserRouter and root base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx';
@@ -8,13 +8,13 @@ import { QueryProvider } from './context/QueryContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <QueryProvider>
         <AppProvider>
           <App />
         </AppProvider>
       </QueryProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
 
